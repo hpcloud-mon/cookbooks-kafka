fab643f4af3b0e9a5260458037f0bee98879e72a
Tim Kuhlman <tim.kuhlman@hp.com>
2014-08-27 15:52:32 -0600

    Fixed situation of kafka coming up before zookeeper is ready.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'hpcs-mon-som@hp.com'
 license          'All rights reserved'
 description      'Installs/Configures kafka'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 depends          "ark"
 depends          "hostsfile"
